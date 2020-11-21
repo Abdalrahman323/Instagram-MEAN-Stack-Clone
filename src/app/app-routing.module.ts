0846004e5804d5ac8e1ef28134137512b84d9d2d
Abdalrhman <abdalrhmann323@gmail.com>
2020-11-21 13:12:55 +0200

    add create post - post list compoenents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CreatePostComponent } from './create-post/create-post.component';
+import { PostsComponent } from './posts/posts.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'',component:PostsComponent},
+{path:'create',component:CreatePostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 
 
@@ -11,10 +14,13 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     HeaderComponent,
+    PostsComponent,
+    CreatePostComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularSvgIconModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
