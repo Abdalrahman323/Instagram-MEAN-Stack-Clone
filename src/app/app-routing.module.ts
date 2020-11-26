238af2d32e1a6190ee3a55a1241bc29f494ed51f
Abdalrhman <abdalrhmann323@gmail.com>
2020-11-26 15:45:18 +0200

    add user profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { CreatePostComponent } from './create-post/create-post.component';
 import { PostsComponent } from './posts/posts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 const routes: Routes = [
 {path:'',component:PostsComponent},
-{path:'create',component:CreatePostComponent}
+{path:'create',component:CreatePostComponent},
+{path:"user",component:UserProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PostsComponent } from './posts/posts.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { PostComponent } from './post/post.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
@@ -18,6 +19,7 @@ import { PostComponent } from './post/post.component';
     PostsComponent,
     CreatePostComponent,
     PostComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
