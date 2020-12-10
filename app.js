452715816010c5ff17b445e5ddb14cb6008c6d86
unknown <Abdalrhmann323@gmail.com>
2020-12-10 11:44:16 +0200

    fix-port-issue

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const app =express();
 const postsRoutes = require('./routes/posts');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT || "3000";
 
 
-//  Related to mongoDatabase
+//const URL = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.44hz6.mongodb.net/${process.env.MONGO_DB_Name}`;
+//local connection
 const URL = "mongodb://localhost:27017/instagram_db";
+
+
 mongoose.connect(URL,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useFindAndModify:false
 }).then(()=>{
-  console.log('connected Successfully');
+  console.log(`connected Successfully to ${process.env.MONGO_DB_Name}`);
 }).catch(()=>{
   console.log('Connection failed !!')
 })
@@ -29,5 +32,5 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use("/api/posts",postsRoutes);
 
 app.listen(PORT,()=>{
-  console.log("Server in running on port" + PORT)
+  console.log("Server in running on port " + PORT)
 })
