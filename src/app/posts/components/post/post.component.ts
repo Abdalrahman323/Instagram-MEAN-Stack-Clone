53cb760b4fac4bcf3946c01dceef23a64879d1a0
unknown <Abdalrhmann323@gmail.com>
2020-12-15 11:05:01 +0200

    fix UI issues

--- src/app/posts/components/post/post.component.ts
+++ src/app/posts/components/post/post.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { Post } from '../../post.model';
 
 @Component({
@@ -11,19 +11,14 @@ export class PostComponent implements OnInit {
 
   @Input('post') post :Post;
 
-  constructor( private sanitizer: DomSanitizer) { }
+  constructor( private router:Router) { }
 
   ngOnInit(): void {
 
-    // let TYPED_ARRAY = new Uint8Array(this.post.photo.data);
-    // const STRING_CHAR = TYPED_ARRAY.reduce((data, byte)=> {
-    //   return data + String.fromCharCode(byte);
-    //   }, '');
-    // let base64String = btoa(STRING_CHAR);
-
-     //this.post.photo= 'data:image/jpeg;base64,' + this.post.photo;
-    // this.post.photo = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + this.post.photo);
-
+ 
+  }
+  navigateToUser(userId:string){
+  this.router.navigate(['/user/'+userId]);
   }
 
 }
--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -1,5 +1,5 @@
 export interface Post {
   title: string;
   photo: any;
-  postedBy?:string;
+  postedBy?:any;
 }
